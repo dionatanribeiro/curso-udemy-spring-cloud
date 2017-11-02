607f5220d3cf010d591f1d1b943d002bedab7e54
Dionatan Ribeiro <dionatangil@gmail.com>
2017-11-02 12:44:17 -0200

    adicionar testes de i18n

--- src/main/java/com/cursospringcloud/restful/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/cursospringcloud/restful/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,13 @@ package com.cursospringcloud.restful.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,4 +16,26 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	/**
+	 * O nome do método deve ser messageSource para poder ser utilizado este Bean
+	 * automaticamente na instância de messageSource quando for instanciada via @Autowired.
+	 * Outra alternativa é informar o name pela propriedade @Bean assim é possível
+	 * manter o método com um nome diferente.
+	 *
+	 * Ex: @Bean(name = "messageSource")
+	 */
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+
 }
